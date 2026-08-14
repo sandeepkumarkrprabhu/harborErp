d9198bf815c3ac217774903b61cd262216267f32
sandeep <sandeepkumarkrprabhu@gmail.com>
2026-08-14 16:07:27 +0530

    fixes for source dispaly  in project card tile

--- src/app/shared/components/project-card/project-card.ts
+++ src/app/shared/components/project-card/project-card.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Project } from '../../../Models/project'; 
+import { Project } from '../../../Models/project';
 import { getProjectStatus } from '../../../features/utils/string-utils';
 import { Badge } from '../badge/badge';
 
@@ -12,10 +12,13 @@ import { Badge } from '../badge/badge';
   templateUrl: './project-card.html',
   styleUrls: ['./project-card.css'],
 })
-export class ProjectCard {
-  @Input() project!: Project; 
+export class ProjectCard implements OnInit {
+  @Input() project!: Project;
 
-  /** Calculate progress percentage for healthy/unhealthy */
+  ngOnInit(): void {
+    console.log('Project received:', this.project);
+  }
+
   get healthyPercent(): number {
     const total = this.project.healthy + this.project.unhealthy;
     return total > 0 ? (this.project.healthy / total) * 100 : 0;
@@ -26,10 +29,11 @@ export class ProjectCard {
     return total > 0 ? (this.project.unhealthy / total) * 100 : 0;
   }
 
-  /** Status color for the circle */
+  get source(): string {
+    return this.project.source;
+  }
+
   get statusColor(): string {
-    
-      return getProjectStatus(this.project.healthy, this.project.unhealthy, this.project.status);
-    
+    return getProjectStatus(this.project.healthy, this.project.unhealthy, this.project.status);
   }
 }
